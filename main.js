1de6d153346017c8a7152b8a8db8f743e8babe7d
RC-NETWORK879 <131510324+RC-NETWORK879@users.noreply.github.com>
2023-06-08 21:51:30 +0545

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ let luck = 0;
 let max_click = 60;
 let box = document.getElementById('box');
 let content = [1,2,3,4,5,6];
+let audio = new Audio('images/dice.mp3');
 // let luck_board = document.getElementById('per');
 // let score_board = document.getElementById('score');
 
 function roll(){
+  audio.play();
+  box.innerHTML = `<img class="dice" src="images/loader.gif">`;
+setTimeout(()=>{
   click = click-1;
   let ind = Math.floor(Math.random()*6);
   if (click>=0) {
@@ -22,6 +26,7 @@ function roll(){
   box.innerHTML += `<h6 class='score'>Score: ${score} </h6>`;
   box.innerHTML +=`<h6 class='score-luck'>Luck: ${luck}%</h6>`;
   console.log(ind);
+},1500)
 }
 window.onload = function (){
   box.innerHTML += `<h6 class='score'>Score: ${score} </h6>`;
@@ -29,5 +34,6 @@ window.onload = function (){
   box.innerHTML += `<img class="dice" src='images/${content[3]}.png'>`;
     box.innerHTML +=`<h6 class='chance'>You have 10 chance</h6>`;
 }
+
 
 
