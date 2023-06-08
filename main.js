a49571cf1ca3df2263aebf10020782d84c7bcf57
RC-NETWORK879 <131510324+RC-NETWORK879@users.noreply.github.com>
2023-06-08 16:41:17 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let score = 0;
+let click = 10;
+let luck = 0;
+let max_click = 60;
+let box = document.getElementById('box');
+let content = [1,2,3,4,5,6];
+// let luck_board = document.getElementById('per');
+// let score_board = document.getElementById('score');
+
+function roll(){
+  click = click-1;
+  let ind = Math.floor(Math.random()*6);
+  if (click>=0) {
+    box.innerHTML = `<img class="dice" src='images/${content[ind]}.png'>`;
+    box.innerHTML +=`<h6 class='chance'>You have ${click} more chance</h6>`;
+  score = score+content[ind];
+  }else{
+     alert('You are ' + luck + '% lucky');
+    
+  }
+   luck =(score/60*100).toFixed(2);
+  box.innerHTML += `<h6 class='score'>Score: ${score} </h6>`;
+  box.innerHTML +=`<h6 class='score-luck'>Luck: ${luck}%</h6>`;
+  console.log(ind);
+}
+window.onload = function (){
+  box.innerHTML += `<h6 class='score'>Score: ${score} </h6>`;
+  box.innerHTML += `<h6 class='score-luck'>Luck: ${luck}%</h6>`;
+  box.innerHTML += `<img class="dice" src='images/${content[3]}.png'>`;
+    box.innerHTML +=`<h6 class='chance'>You have 10 chance</h6>`;
+}
+
+
